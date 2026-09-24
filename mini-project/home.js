7611ce908530c6530901a94618f73d32eab98aff
KARTHIK <karthikodungattuthodi@gmail.com>
2026-09-25 02:26:15 +0530

    update to crave & co.

--- mini-project/home.js
+++ mini-project/home.js
@@ -1,23 +1,4 @@
-const base_api = "https://dummyjson.com";
 
-const get_api = (end_point) => {
-    return base_api + end_point;
-}
-
-const request = async (url) => {
-    try {
-        const response = await fetch(url);
-        return await response.json();
-    } catch {
-        return null;
-    }
-}
-
-const getAllRecipes = async () => {
-    const api = get_api("/recipes?skip=&limit=4")
-    const response = await request(api)
-    return response.recipes;
-}
 
 const popular = document.getElementById("popular-dishes");
 
